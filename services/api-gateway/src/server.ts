79214980e9df34e4ae11ed74b73286791f815ee9
edisonyls <edisonyls@hotmail.com>
2025-07-16 23:47:04 +1000

    chore: change to test cicd

--- services/api-gateway/src/server.ts
+++ services/api-gateway/src/server.ts
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:3001",
-      "http://localhost:3001", // Next.js dev server
+      "http://localhost:3001",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
